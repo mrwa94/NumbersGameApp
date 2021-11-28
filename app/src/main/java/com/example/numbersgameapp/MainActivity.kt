aabb3af51e6f99a4e60b9a2c73c6c5d63bdea43c
Ayman alsubhi <aymanalsubhi@MacBook-Pro-alkhas-b-Ayman.local>
2021-11-29 02:11:16 +0300

    Initial commit

---
+++ app/src/main/java/com/example/numbersgameapp/MainActivity.kt
@@ -0,0 +1,111 @@
+package com.example.numbersgameapp
+
+import android.content.DialogInterface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var clRootNumbersGame: ConstraintLayout
+    private lateinit var recyclervc : RecyclerView
+    private lateinit var guessField: EditText
+    private lateinit var guessButton: Button
+    private lateinit var messages: ArrayList<String>
+
+    private var answer = 0
+    private var guesses = 3
+    private var newGame = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        answer = Random.nextInt(10)
+
+        clRootNumbersGame = findViewById(R.id.constrait)
+        messages = ArrayList()
+
+        recyclervc.adapter = MessageAdapter(this, messages)
+        recyclervc.layoutManager = LinearLayoutManager(this)
+
+        guessField = findViewById(R.id.EditText)
+        guessButton = findViewById(R.id.button)
+
+        guessButton.setOnClickListener { addMessage() }
+
+        title = "Numbers Game"
+    }
+
+    private fun addMessage(){
+        val msg = guessField.text.toString()
+        if(msg.isNotEmpty()){
+            if(guesses>0){
+                if(msg.toInt() == answer){
+                    disableEntry()
+                    CustomAlertDialog("You win!\n\nPlay again?")
+                }else{
+                    guesses--
+                    messages.add("You guessed $msg")
+                    messages.add("You have $guesses guesses left")
+                }
+                if(guesses==0){
+                    disableEntry()
+                    messages.add("You lose - The correct answer was $answer")
+                    messages.add("Game Over")
+                    CustomAlertDialog("You lose...\nThe correct answer was $answer.\n\nPlay again?")
+                }
+            }
+            guessField.text.clear()
+            guessField.clearFocus()
+            recyclervc.adapter?.notifyDataSetChanged()
+        }else{
+            Snackbar.make(clRootNumbersGame, "Please enter a number", Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun CustomAlertDialog( message: String) {
+        // build alert dialog
+        val dialogBuilder = AlertDialog.Builder(this)
+
+        // set message of alert dialog
+        dialogBuilder.setMessage(message)
+            // if the dialog is cancelable
+            .setCancelable(false)
+            // positive button text and action
+            .setPositiveButton("Yes", DialogInterface.OnClickListener {
+                    dialog, id -> recreate()
+            })
+            // negative button text and action
+            .setNegativeButton("No", DialogInterface.OnClickListener {
+                    dialog, id -> dialog.cancel()
+            })
+
+        // create dialog box
+        val alert = dialogBuilder.create()
+        // set title for alert dialog box
+        alert.setTitle("Game Over")
+        // show alert dialog
+        alert.show()
+    }
+
+    private fun disableEntry(){
+        guessButton.isEnabled = false
+        guessButton.isClickable = false
+        guessField.isEnabled = false
+        guessField.isClickable = false
+    }
+
+    override fun recreate() {
+        super.recreate()
+        answer = Random.nextInt(10)
+        guesses = 3
+        messages.clear()
+    }
+
+}
